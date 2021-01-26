f746bd7486df15182ef4889c4c193c1edd7f75c4
shiqi <shiqi.tong@thoughtworks.com>
2021-01-27 00:11:34 +0800

    problem2 serialize Person class

--- src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
@@ -1,16 +1,31 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.IOException;
+
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class PersonVo {
 
   private String id;
   private Integer age;
   private String name;
+  @JsonInclude(JsonInclude.Include.NON_NULL)
   private String hobby;
+
+  public PersonVo(String id, Integer age, String name, String hobby) {
+    this.id = id;
+    this.age = 0;
+    if(age!=null)this.age = age;
+    this.name = name;
+    this.hobby = hobby;
+  }
 }
